9e66f12ba8647fcee617f44af2b2c4bac0b992ca
fjeff503 <fjeffersonpineda@hotmail.com>
2020-10-13 16:59:50 -0600

    agregando docs

---
+++ docs/precache-manifest.7d0d9fcdfabf34e976972b22ae46c864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63cf69c079980df28e99433403116c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17ca5c90c6597ea29fb8",
+    "url": "/static/css/main.94ee9994.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ca5c90c6597ea29fb8",
+    "url": "/static/js/main.52e1f5c5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
